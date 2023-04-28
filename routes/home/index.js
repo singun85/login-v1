7725af43a986203a3f7746c6992630cb5cff0fc2
singun85 <singun85@gmail.com>
2023-04-28 13:07:39 +0900

    초기화

---
+++ routes/home/index.js
@@ -0,0 +1,11 @@
+"use strict";
+//모듈
+const express = require("express")
+const router = express.Router();
+
+const ctrl = require("./home.ctrl"); //home.ctrl.js 의 hello,login 값을 가져와
+
+router.get("/", ctrl.hello); //home.ctrl.js 에 있는 hello 를 불러와
+router.get("/login", ctrl.login);//home.ctrl.js 에 있는 login 을 불러와
+
+module.exports = router; //중요! 각 js 파일에 모듈은 exports 해줘야 다른 경로에서 사용가능!
