ada440ae450bdfaebdaf2764802c3ec78673c180
singun85 <singun85@gmail.com>
2023-05-02 22:22:40 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,13 +1,42 @@
 "use strict";
 
-const hello = (req, res) => {
-    res.render("home/index"); //router 에 입력된 콜백 함수 render경로를 실행해.
-};
-const login = (req,res) => { //(요청)도메인에 /login 요청이  들어왔을때
-    res.render("home/login"); //(응답)router 에 입력된 콜백 함수 render경로를 실행해.
+
+
+const output = {
+    home: (req, res) => {
+        res.render("home/index"); //router 에 입력된 콜백 함수 render경로를 실행해.
+    },
+    login: (req,res) => { //(요청)도메인에 /login 요청이  들어왔을때
+        res.render("home/login"); //(응답)router 에 입력된 콜백 함수 render경로를 실행해.
+    },
 };
 
+const users = {
+    id: ["cokaking", "singun", "pppip"],
+    psword: ["1234", "12345", "123456"],
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+        
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg:"로그인에 실패하였습니다.",
+        });
+    },
+};
 module.exports ={ //중요! 각 js 파일에 모듈은 exports 해줘야 다른 경로에서 사용가능!
-    hello,
-    login,
+    output,
+    process,
 }
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -6,7 +6,9 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl"); //home.ctrl.js 의 hello,login 값을 가져와
 
-router.get("/", ctrl.hello); //home.ctrl.js 에 있는 hello 를 불러와
-router.get("/login", ctrl.login);//home.ctrl.js 에 있는 login 을 불러와
+router.get("/", ctrl.output.home); //home.ctrl.js 에 있는 hello 를 불러와
+router.get("/login", ctrl.output.login);//home.ctrl.js 에 있는 login 을 불러와
+router.post("/login", ctrl.process.login);
+
 
 module.exports = router; //중요! 각 js 파일에 모듈은 exports 해줘야 다른 경로에서 사용가능!
